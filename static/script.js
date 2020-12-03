da0af5f4c0c88ad1713bbf9ddc5cf210ce395250
anunay sinha <anusu90@gmail.com>
2020-12-03 17:31:07 +0530

    added looping

--- static/script.js
+++ static/script.js
@@ -3,8 +3,9 @@ let url = 'https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&typ
 startBtn =  document.querySelector('#play');
 highScroreBtn =  document.querySelector('#high-score');
 
-questionClassArray =[];
-playerClassArray = [];
+var questionClassArray =[];
+var playerClassArray = [];
+var globalQuesdata;
 
 mainContainer = document.querySelector('#main-container');
 
@@ -23,21 +24,36 @@ class Player {
     }
 }
 
+var questionCount = {
+    'count': 1,
+};
+
 checkanswer = (questionObject, selectedOption) => {
     if (selectedOption === questionObject.correctAnswer){
         tempPlayer.score += 10;
+        questionCount.count = questionCount.count + 1;
+    } else {
+        questionCount.count = questionCount.count + 1;
+    }
+
+    if (questionCount.count<=10){
+        console.log('i am here')
+        console.log(questionCount);
+        displayHTML(questionClassArray[questionCount.count - 1], tempPlayer, questionCount.count);
+    } else {
+        console.log('it is the end')
     }
 
     console.log(tempPlayer);
 }
 
-displayHTML = (questionObject, player, questionCount) =>{
+displayHTML = (questionObject, player) =>{
     console.log('hh', questionObject);
     mainContainer.innerHTML =  `     <div class="row h-100 align-items-center">` +
 `    <div class="col-sm-12 col-lg-6 offset-lg-3 display-col">` +
 `        <div class="row">`+
 `            <div class="col-6">`+
-`            <p>Question ${questionCount}/10</p>`+
+`            <p>Question ${questionCount.count}/10</p>`+
 `           <div class="progress w-50">`+
 `                <div class="progress-bar my-progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" id="prog-inner-bar"></div>`+
 `            </div>`+
@@ -59,38 +75,36 @@ displayHTML = (questionObject, player, questionCount) =>{
 `    </div>`+
 `</div>`
 
+    optionBtns = document.querySelectorAll('.option-btn').forEach((btn) => {
+        btn.addEventListener('click' ,(e)=> {
+            console.log(e.target.value);
+            checkanswer(questionObject,e.target.value);
+        })
+    })
+
 }
 
 
-async function getQuestions (url) {
+async function startGame (url) {
     let res =  await fetch(url);
-    let quesData = await res.json();
+    globalQuesdata = await res.json();
 
-    quesData.results.forEach((v) => {
+    globalQuesdata.results.forEach((v) => {
         questionClassArray.push(new ChoiceText(v.question, v.correct_answer, v.incorrect_answers))
     });
-
-    let questionCount = 1;
 
     tempPlayer = new Player();
     console.log(tempPlayer);
     console.log(questionClassArray);
 
+    questionCount.count = 1;
+    console.log(questionCount);
+
+    displayHTML(questionClassArray[questionCount.count - 1], tempPlayer);
+
+}
     
 
-    displayHTML(questionClassArray[0], tempPlayer, questionCount);
-
-    optionBtns = document.querySelectorAll('.option-btn').forEach((btn) => {
-        btn.addEventListener('click' ,(e)=> {
-            console.log(e.target.value);
-            checkanswer(questionClassArray[0],e.target.value);
-        })
-    })
-
-
-    sessionStorage.setItem('myQuestions', JSON.stringify(questionClassArray))
-}
-
 startBtn.addEventListener('click', ()=>{
-    getQuestions(url);
+    startGame(url);
 })
